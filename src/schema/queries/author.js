4b38119d196352fd65bc0bd43bd9be06110a1e91
Dante Garcia <dantegarciaram93@gmail.com>
2017-11-27 05:34:51 -0800

    added post query

--- src/schema/queries/author.js
+++ src/schema/queries/author.js
@@ -1,14 +1,16 @@
-import { GraphQLList } from 'graphql';
-import { Post } from '../types/Post'
+import { GraphQLString, GraphQLNonNull } from 'graphql';
+import { Author } from '../types/Author'
 import { fakeDatabase } from '../../FakeDatabase'
 
 export default {
   author: {
-    type: new GraphQLList(Post),
+    type: Author,
     description: 'Gets a specific author',
-    args: {},
-    resolve: () => {
-      return fakeDatabase.getBlogPosts();
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLString) }
+    },
+    resolve: (parent, {id}) => {
+      return fakeDatabase.getAuthor(id);
     }
   }
 }
